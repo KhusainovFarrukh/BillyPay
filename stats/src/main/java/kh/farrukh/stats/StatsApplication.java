a8601c29998ee858b6c67e5d52dbfac16c17f147
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-08-18 14:19:07 +0500

    Configured Eureka clients

--- stats/src/main/java/kh/farrukh/stats/StatsApplication.java
+++ stats/src/main/java/kh/farrukh/stats/StatsApplication.java
@@ -2,8 +2,10 @@ package kh.farrukh.stats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StatsApplication {
 
     // TODO: 8/18/22 connect to other services (bill)
