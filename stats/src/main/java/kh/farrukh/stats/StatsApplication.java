69e590c72b60f97b57fbb9fdf22b17468039de0b
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-08-18 11:13:50 +0500

    Connected to db

---
+++ stats/src/main/java/kh/farrukh/stats/StatsApplication.java
@@ -0,0 +1,12 @@
+package kh.farrukh.stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatsApplication.class, args);
+    }
+}
